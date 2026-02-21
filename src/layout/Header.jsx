7f580be623544677bf5c68642a98dcd03ab6b096
Sunnatbek <Sunnatbek912@gmail.com>
2026-02-21 17:59:25 +0500

    done

---
+++ src/layout/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+	return (
+		<>
+		<div className="header">
+
+		<h1 className='logo'>My First App</h1>
+		<ul>
+			<li>
+				<a className='link' href="#">Home</a>
+			</li>
+			<li>
+				<a className='link' href="#">About</a>
+			</li>
+		</ul>
+		</div>
+		</>
+	)
+}
+export default Header;

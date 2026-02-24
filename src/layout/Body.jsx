7c790b68a2cf75f36cd52e9f2685a7f7d015bc4b
Sunnatbek <Sunnatbek912@gmail.com>
2026-02-24 17:56:26 +0500

    oz koldi

--- src/layout/Body.jsx
+++ src/layout/Body.jsx
@@ -1,29 +1,35 @@
 function Body() {
 	return (
-		<div className='body-container'>
+		<>
 			<h3>Mening Loyihalarim</h3>
-			<br />
-			<div className='to-do-app'>
-				<h3 className='todo'>Todo App</h3>
-				<div className='vazifa'>
-					<input className='input' type='text' />
-					<button className='btn-add'>Add</button>
-				</div>
+			<div className='body-container'>
 				<br />
-				<div className='buttons'>
-					<h5>1. Salom </h5>
-					<i class='fa-solid fa-trash'></i>
-				</div>
-				<div className='buttons'>
-					<h5>2. Salom </h5>
-					<i class='fa-solid fa-trash'></i>
-				</div>
-				<div className='buttons'>
-					<h5>3. Salom </h5>
-					<i class='fa-solid fa-trash'></i>
+				<div className='to-do-app'>
+					<h2 className='todo'>Todo App</h2>
+					<div className='vazifa'>
+						<input
+							className='input'
+							type='text'
+							placeholder='Vazifani yozing...'
+						/>
+						<button className='btn-add'>Add</button>
+					</div>
+					<br />
+					<div className='buttons'>
+						<h5>1. Salom </h5>
+						<i class='fa-solid fa-trash'></i>
+					</div>
+					<div className='buttons'>
+						<h5>2. Salom </h5>
+						<i class='fa-solid fa-trash'></i>
+					</div>
+					<div className='buttons'>
+						<h5>3. Salom </h5>
+						<i class='fa-solid fa-trash'></i>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 export default Body
